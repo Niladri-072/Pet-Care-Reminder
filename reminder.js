986c0d8e8659f678a8207a7597c1daeaf1129b6a
Niladri-072 <58231577+Niladri-072@users.noreply.github.com>
2025-05-01 06:33:40 +0530

    Add files via upload

---
+++ reminder.js
@@ -0,0 +1,41 @@
+class Reminder {
+    constructor(name, time) {
+        this.name = name;
+        this.time = time;
+    }
+}
+
+class ReminderManager {
+    constructor() {
+        this.reminders = [];
+    }
+
+    addReminder(name, time) {
+        const reminder = new Reminder(name, time);
+        this.reminders.push(reminder);
+    }
+
+    getReminders() {
+        return this.reminders;
+    }
+
+    deleteReminder(index) {
+        this.reminders.splice(index, 1);
+    }
+
+    saveReminders() {
+        localStorage.setItem('reminders', JSON.stringify(this.reminders));
+    }
+
+    loadReminders() {
+        const storedReminders = localStorage.getItem('reminders');
+        if (storedReminders) {
+            this.reminders = JSON.parse(storedReminders);
+        }
+    }
+}
+
+const reminderManager = new ReminderManager();
+reminderManager.loadReminders();
+
+export { reminderManager };
